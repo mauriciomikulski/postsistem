8fba32e96f88acfe3f225b8245823a02ca2e3c03
Mauricio Mikulski <incognitomikulski@gmail.com>
2021-05-10 23:14:49 -0300

    add incomplete login

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -3,7 +3,7 @@ import styles from './PostForm.module.scss'
 const PostForm = () => {
   return(
     <form>
-      <textarea name="content" className={styles.formContent}></textarea>
+      <textarea name="content" className={styles.formContent} placeholder="Text here..."></textarea>
       <button className={styles.formButton}>Add New Post</button>
     </form>
   )
